aec4414644a9332181a44aefa5e80dcfde69cd9b
Ronny Ancorini <ronnygx@gmail.com>
2019-02-16 22:55:37 +0000

    green: constant_pattern

--- CSharp7Features/Tests.cs
+++ CSharp7Features/Tests.cs
@@ -3,7 +3,7 @@ using NUnit.Framework;
 
 namespace Tests
 {
-    public class Tests {
+    public class OutParametersTests {
         [Test]
         public void out_parameters_one_line_definition() {
             GetCoordinates(out var x, out var y);
@@ -14,6 +14,24 @@ namespace Tests
         private void GetCoordinates(out int x, out int y) {
             x = 3;
             y = 2;
+        }
+    }
+
+    public class PatternMatching {
+        /*
+         * Pattern Types
+           - constants "c"
+           - types "T x"
+           - var
+         * Patterns constructions keywords
+           - is
+           - case
+         */
+
+        [Test]
+        public void constant_pattern () {
+            var x = 3;
+            (x is 3).Should().BeTrue();
         }
     }
 }
